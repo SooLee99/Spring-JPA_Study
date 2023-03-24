16f8267f17c8ba79bf45a3ffb5cd9022ea484ad2
LeeSooo <dltn990930@naver.com>
2023-03-24 10:52:33 +0900

    feat : 기본적인 Spring Boot 생성. (2023-03-23)

---
+++ src/main/java/com/example/spring/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWolrd(){
+        return  "hello-world";
+    }
+}
