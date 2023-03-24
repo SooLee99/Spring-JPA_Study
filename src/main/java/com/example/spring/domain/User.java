22fee25c09536c5e4207d64c513d55c5d65c524e
LeeSooo <dltn990930@naver.com>
2023-03-24 14:56:22 +0900

    feat : 기본적인 Lombok 사용. (2023-03-24)

---
+++ src/main/java/com/example/spring/domain/User.java
@@ -0,0 +1,46 @@
+package com.example.spring.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/* 기본적으로 사용하는 Lombok에 대한 설명. */
+
+/*  각 필드에 접근하려면 객체지향의 기본적인 캡슐화 원칙을 지키기 위해서
+    => @Getter / @Setter 을 만들어줘야함. */
+
+// @Getter
+// @Setter
+
+/* toString() : UserTest.java 테스트코드에 설명 작성. */
+// @ToString
+
+/* JAP 사용 시, Entity 객체를 생성해야 하는 경우가 많이 생김.*/
+
+// @NoArgsConstructor : 인자 없는 기본 생성자 [User user = new User();] 를 선언해주는 역활.
+
+// @AllArgsConstructor : 인자 있는 기본 생성자 [User user1 = new User("martin", "martin@fastcampus", "", "");] 를 선언해주는 역활.
+
+// @RequiredArgsConstructor : final 이나 @NonNull 인 필드 값만 파라미터로 받는 생성자를 만들어줍니다.
+
+// @EqualsAndHashCode : (자주 사용 X)
+// => equals, hashCode를 자동으로 생성해주는 어노테이션.
+//    equals :  두 객체의 내용이 같은지, 동등성(equality) 를 비교하는 연산자
+//    hashCode : 두 객체가 같은 객체인지, 동일성(identity) 를 비교하는 연산자
+//    자바 bean에서 동등성 비교를 위해 equals와 hashcode 메소드를 오버라이딩해서 사용하는데,
+//    @EqualsAndHashCode 어노테이션을 사용하면 자동으로 이 메소드를 생성할 수 있다.
+
+// @Builder
+
+// @Data : @Getter, @Setter, @RequiredArgsConstructor, @ToString, @EqualsAndHashCode를 한번에 설정해주는 어노테이션이다.
+@Data
+public class User {
+    /* 필드: 이름, 이메일 */
+    private String name;
+    private String email;
+
+    /* 필드: 생성 시간, 수정 시간 */
+    // => 일반적으로 jpa 도메인 객체에는 포함이 되어 있어야 한다. => DB 데이터 이력을 관리하기 위함.
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
+}
---
+++ src/test/java/com/example/spring/domain/UserTest.java
@@ -0,0 +1,27 @@
+package com.example.spring.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    @Test
+    void test(){
+        User user = new User();
+        user.setEmail("martin@fastcampus.com");
+        user.setName("martin");
+
+        System.out.println(">>> " + user);
+        // => (문제) 위의 테스트 결과 값을 보면
+        //          해당 객체의 내용이 아닌, 클래스의 해시코드를 확인할 수 있다.
+
+        // => (결론) 이름과 이메일이 객체에 재대로 들어갔는지 확인할 수가 없다.
+
+        // => (원인) 모든 자바 클래스는 Object 클래스의 하위 객체이다.
+        //          Object 클래스에서 toString() 메소드를 살펴보면,
+        //          getClass().getName() + "@" + Integer.toHexString(hashCode());
+        //          암묵적으로 toString()이 실행되기 때문에 이와 같은 결과가 나옴.
+
+        // => (해결방안) User 클래스에서 toString()을 오버라이딩 하면 된다.
+    }
+}
